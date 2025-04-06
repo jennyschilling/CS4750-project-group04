906ab1aefc515799cf9802549df12ebe8a98f66e
jennyschilling <xdj3kg@virginia.edu>
2025-04-06 15:41:58 -0400

    more data routes added

--- backend/data_routes.py
+++ backend/data_routes.py
@@ -3,11 +3,22 @@ from db_config import get_db_connection
 
 data_route = Blueprint('data', __name__)
 
-@data_route.route('/race-history')
+@data_route.route('/race-result')
 def race_history():
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute("SELECT * FROM RaceResult;")
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    return jsonify(rows)
+
+@data_route.route('/teams')
+def get_teams():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Team;")
     rows = cur.fetchall()
     cur.close()
     conn.close()
